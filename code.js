9bf088c2e63f0d91b0fccd24436c5fb22a476a1a
Krystal <krystal.briggs@kenzie.academy>
2021-01-05 18:07:29 -0500

    pushing js and html

---
+++ code.js
@@ -0,0 +1,14 @@
+let gotCitiesCSV =
+  "King's Landing,Braavos,Volantis,Old Valyria,Free Cities,Qarth,Meereen";
+let lotrCitiesArray = [
+  "Mordor",
+  "Gondor",
+  "Rohan",
+  "Beleriand",
+  "Mirkwood",
+  "Dead Marshes",
+  "Rhun",
+  "Harad",
+];
+let bestThing =
+  "The best thing about a boolean is even if you are wrong you are only off by a bit";
